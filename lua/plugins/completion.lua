c5d152059d825ead9987da216833e7442d269afe
multimedia22x@gmail.com <multimedia22x@gmail.com>
2025-01-04 20:46:40 +0100

    bla bla

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,17 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      menu = { border = "rounded" },
+      documentation = { window = { border = "rounded" } },
+    },
+    signature = { window = { border = "rounded" } },
+    keymap = {
+      ["<C-s>"] = {
+        function(cmp)
+          cmp.show({ providers = { "snippets" } })
+        end,
+      },
+    },
+  },
+}
