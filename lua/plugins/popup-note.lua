186381379159a009268a6cc7af852f0444773922
multimedia22x@gmail.com <multimedia22x@gmail.com>
2025-12-21 14:29:35 +0100

    notes

--- lua/plugins/popup-note.lua
+++ lua/plugins/popup-note.lua
@@ -1,5 +1,5 @@
 return {
-  -- popup script --
+  -- popup script combined with leader N within vim
   vim.keymap.set("n", "<leader>N", function()
     require("lazy.util").float_term({ "notepop.sh" })
   end, { desc = "Popup notes" }),
